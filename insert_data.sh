0a163d729d0468e54455c9cd07aaa473c5b93e86
Sleia67 <108318302+Sleia67@users.noreply.github.com>
2023-08-08 13:30:19 -0500

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -17,16 +17,16 @@ do
   WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
   OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
 
-  # Insert new teams if not found
+  # Insert if not found
   if [[ -z $WINNER_ID ]]; then
     INSERT_WINNER=$($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
     if [[ $INSERT_WINNER == "INSERT 0 1" ]]; then
       echo Inserted into teams, $WINNER
     fi
-    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'") 
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
   fi
 
-  if [[ -z $OPPONENT_ID ]]; then
+  if [[ -z $OPPONENT_ID ]]; then  
     INSERT_OPPONENT=$($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
     if [[ $INSERT_OPPONENT == "INSERT 0 1" ]]; then
       echo Inserted into teams, $OPPONENT
