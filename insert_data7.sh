2a549646de945c020c5f1e7621253661dfe85e61
Sleia67 <108318302+Sleia67@users.noreply.github.com>
2023-08-08 14:23:24 -0500

    Update insert_data7.sh
    #! /bin/bash
    
    if [[ $1 == "test" ]]
    then
      PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
    else
      PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
    fi
    
    # Do not change code above this line. Use the PSQL variable above to query your database.

--- insert_data7.sh
+++ insert_data7.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-PSQL="psql -X --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+#! /bin/bash
 
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
 # Add year column to games table
 $($PSQL "ALTER TABLE games ADD COLUMN year integer;")
 
